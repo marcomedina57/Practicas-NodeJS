4ec51e8fa5dfe65c8c58abdcf052e74027a5211b
Marco Medina <marcos57@hotmail.com>
2021-06-09 13:37:23 -0600

    Cambios

--- app.js
+++ app.js
@@ -1,50 +1,23 @@
+const express = require('express')
+const app = express()
+ 
 
-const colors = require('colors');
+// Servir contenido estatico
 
-const {
-    inquireMenu, 
-    inquirerMenu,
-    leerInput,
-    pausa} = require('./helpers/inquirer');
-const Tarea = require('./models/tarea');
-const Tareas = require('./models/tareas');
-console.clear();
-
-const main = async() => {
-    console.log('Hola mundo');
-    
-            const tareas = new Tareas();
-    let opt = '';
-    do {
-        
-
-        // tareas._listado[tarea.id] = tarea;
-
-        // console.log(tareas);
-        // console.log(tarea);
-
-        opt = await inquirerMenu();
-
-       switch(opt) {
-           case '1':
-               const desc = await leerInput('Descripcion: ');
-               tareas.crearTarea(desc);
-               //crear opcion
-            break;
-
-            case '2':
-                console.log(tareas.listadoArr[0]);
-            break;
-       }
-
-       await pausa();
-
-    } while(opt !== '0'){
-        
-    }
-
-}
+app.use(express.static('public'));
 
 
 
-main();
+app.get('/generic', function(req, res){
+    res.sendFile(__dirname + '/public/generic.html')
+});
+
+app.get('/elements', function(req, res){
+    res.sendFile(__dirname + '/public/elements.html')
+});
+
+app.get('*', function(req, res){
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(8080)
