ae12570f6d41ec6d7a0d49165fd753da68c956bd
Marco Medina <marcos57@hotmail.com>
2021-06-09 17:50:16 -0600

    cambios despliegue

--- app.js
+++ app.js
@@ -9,31 +9,31 @@ const port = process.env.PORT;
 
 app.use(express.static('public'));
 
-app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials', function(err){
+// app.set('view engine', 'hbs');
+// hbs.registerPartials(__dirname + '/views/partials', function(err){
 
-});
+// });
 
-app.get('/', function(req, res){
-    res.render('home', {
-        nombre: 'Fernando Herrera',
-        titulo: 'Curso de Node'
-    });
-});
+// app.get('/', function(req, res){
+//     res.render('home', {
+//         nombre: 'Fernando Herrera',
+//         titulo: 'Curso de Node'
+//     });
+// });
 
 
-app.get('/generic', function(req, res){
-    res.render('generic', {
-        nombre: 'Generic',
-        titulo: 'Ponte nuevo'
-    })
-});
-app.get('/elements', function(req, res){
-    res.render('elements', {
-        nombre: 'Elements',
-        titulo: 'Ponte nuevo'
-    })
-});
+// app.get('/generic', function(req, res){
+//     res.render('generic', {
+//         nombre: 'Generic',
+//         titulo: 'Ponte nuevo'
+//     })
+// });
+// app.get('/elements', function(req, res){
+//     res.render('elements', {
+//         nombre: 'Elements',
+//         titulo: 'Ponte nuevo'
+//     })
+// });
 
 // app.get('/elements', function(req, res){
 //     res.sendFile(__dirname + '/public/elements.html')
