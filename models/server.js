37596c4fdb423f6b0090287d6fc0d6868c785385
Marco Medina <marcos57@hotmail.com>
2021-06-10 23:17:04 -0600

    cambios marco conexion

--- models/server.js
+++ models/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
+
 
 class Server {
 
@@ -7,10 +9,18 @@ class Server {
         this.app = express();
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
+
+        // Conectar a base de datos
+        this.conectarDB();
+
         //Middlewares
         this.middlewares();
 
         this.routes();
+    }
+
+    async conectarDB(){
+        await dbConnection();
     }
 
     middlewares(){
